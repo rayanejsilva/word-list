0e7647db0de51a624f5cabf352de36d5880c9526
Rayane Silva <rayanej.silva@gmail.com>
2021-08-13 14:28:44 +0200

    update index.css

--- client/src/components/render-list.js
+++ client/src/components/render-list.js
@@ -1,12 +1,20 @@
-// document this function!
+/* eslint-disable spellcheck/spell-checker */
+/**
+ * Renders an array of strings.
+ *
+ * @param {string[]} [toRender=[]] - an array of strings to render into the UI.
+ * @returns {string[]} - a rendered words list.
+ */
 export const renderList = (toRender = []) => {
+  // Create an unordered list
   const ulEl = document.createElement('ul');
 
   for (const nextItem of toRender) {
+    // Create the list item
     const liEl = document.createElement('li');
     liEl.innerText = nextItem;
+    // Place the item inside of the list
     ulEl.appendChild(liEl);
   }
-
   return ulEl;
 };
